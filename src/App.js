d80fe49ea0d21cb86328ec96a6910fb3ff21da67
Keith Lockey <lockey@gmail.com>
2017-04-07 22:26:23 -0400

    add memory first time

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+ state = {
+    names: ['run','jump', 'play', 'dog', 'person', 'frog',
+            'cow','cat', 'cat', 'cow','frog','person','dog',
+            'play','jump','run'],
+    matched: [],
+    picks: []
+ }
+
+
+ check = () => {
+            console.log('in check')
+           if (this.state.names[this.state.picks[0]] === this.state.names[this.state.picks[1]]) {
+              this.setState ({
+              //  console.log('in set state');
+                matched: [...this.state.matched, this.state.picks[1]]
+              })
+            }
+            this.state.picks = []
+  }
+
+ choose = (positionOfCard) => {
+   console.log(positionOfCard)
+   if (this.state.picks.length === 2) {
+     
+   }else {
+    this.setState({
+      picks: [...this.state.picks, positionOfCard]
+    }, () => {
+      if (this.state.picks.length === 2) {
+          this.check()
+      }
+    })
+   }
+  }
+
+render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <NameList names={this.state.names} handleClick={this.choose} />
+    )
   }
 }
+
+const NameList = (props) => {
+  const listNames = props.names.map((name, i) => {
+    return <li key={i}>
+      <button onClick={() => props.handleClick(i)} id={name}>
+        {name}
+      </button>
+    </li>
+  })
+ return <ul>{listNames}</ul>
+}
+
+
+
+
+
+
+
+
 
 export default App;
